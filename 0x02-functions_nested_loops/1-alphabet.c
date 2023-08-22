a9390059fc079e4452696f42ae1a75f68819508e
Kingdostymusic Hub <jaydosty@gmail.com>
2023-08-21 20:11:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
- *
- * Return:void
+ * print_alphabet - print all alphabet in lowercase
  */
 
 void print_alphabet(void)
 {
-char ch;
-for (ch = 'a '; ch <= 'z'; ch++)
-_putchar(ch);
-_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
